5b65f161cf7a67ab6c8aded74c03a5234b0d4937
Loïc Stringer <loic.stringer@gmail.com>
2021-05-01 14:45:30 +0200

    Initial commit

---
+++ src/test/java/com/diabgnozscreennotesservice/DiabgnozscreenNotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diabgnozscreennotesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabgnozscreenNotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
